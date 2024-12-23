2b8d51e20c2eccc81f427ebf87e5538a38272574
daniyar.amangeldy <d.amangeldy@arbuz.kz>
2024-12-23 15:42:53 +0500

    Second commit

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ FROM openjdk:17-jdk-slim
 EXPOSE 8080:8080
 RUN mkdir /app
 COPY --from=build /home/gradle/src/build/libs/*.jar /app/app.jar
-ENTRYPOINT ["java","-jar","/app/app.jar"]
+ENTRYPOINT ["java", "-jar", "/app/app.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.serialization") version "1.9.0"
+    application
     id("io.ktor.plugin") version "2.3.3"
 }
 
@@ -11,10 +12,25 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("com.example.ApplicationKt") // Указываем главный класс
+}
+
 dependencies {
     implementation("io.ktor:ktor-server-core:2.3.3")
     implementation("io.ktor:ktor-server-netty:2.3.3")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.3")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.3")
     implementation("ch.qos.logback:logback-classic:1.4.11")
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes["Main-Class"] = "com.example.ApplicationKt"
+        }
+        // Включаем все зависимости в JAR
+        from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
 }
